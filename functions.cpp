d084050637f6ae2e7031a5d65558f6f7d2f33a90
bsee25047 <bsee25047@itu.edu.pk>
2026-03-30 12:27:55 +0500

    bsee25047

--- functions.cpp
+++ functions.cpp
@@ -3,10 +3,19 @@
 
 int sumOfSquares(int n) 
 {
+    int sum =0;       //decarling and iniliazitating
+    if (n == 0) {     //using if statement
+        return 0; 
+    }
+    return n * n + sumOfSquares(n - 1);     //recursive case
 }
 
-int findFactorial(int n)
+int findFactorial(int num)
 {
+    if (num == 0 || num == 1) {    //using if statement
+        return 1;   
+    }
+    return num * findFactorial(num - 1);    //recursive case
 }
 
 
--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 
 
 int main() {
-   
+ int n;
+ cout<<"Enter a number ";   //getting input from userr
+ cin>>n;
+ cout<<"Sum of square "<<sumOfSquares(n)<<endl;   //calling function
+
+int num;
+cout << "Enter a number: ";   //getting input from user
+cin >> num;
+cout << "Factorial is " << findFactorial(num) << endl;   //calling function
+
+return 0;
 }
